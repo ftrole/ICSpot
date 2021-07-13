0d08824e7cfb04992578f716e49a8dad1ce61eae
ftrole <File69>
2021-07-13 23:38:17 +0200

    "added TODO list"

---
+++ TODO/TODO.md
@@ -0,0 +1,3 @@
+# To Do List
+
+- [ ] find an elegant way to fix the honeyd log of port 8000 which generates a packet storm. For now the implemented solution consists in running the [clean-log.py](./clean-log.py) script on a normal log file.
---
+++ TODO/clean_log.py
@@ -0,0 +1,30 @@
+# run this script in order to obtain a honeyd log file cleaned 
+# from the packet storm on port 8000 caused by the HMI periodical refresh
+# you must provide honeyd.log as argument and will obtain honeyd_clean.log as result
+
+
+#!/usr/bin/env python3
+import sys
+import os
+
+analyzed=0
+prev_line=""
+
+logFile = sys.argv[1]
+with open(logFile) as f:
+    logList = f.readlines()
+
+f= open("honeyd_clean.log","w+")
+
+for line in logList:
+    # parse every line
+    analyzed = analyzed + 1
+    if not "8000" in line or not "E" in line or not "8000" in prev_line or not "E" in prev_line:
+    # write line in the clean log file
+      f.write(line)
+    prev_line=line
+
+f.close()
+    	        
+os.system('clear')
+print("Analyzed " + str(analyzed) + " log liness)
