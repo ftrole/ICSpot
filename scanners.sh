8a0fee7133807b8b5f26d3bf43856307c73dcfd3
FedezT <federico.turrin94@gmail.com>
2022-12-09 20:53:32 +0100

    - Fix ftp service - Improved scan script

--- scanners.sh
+++ scanners.sh
@@ -1,18 +1,27 @@
+echo "Enter the target IP: "  
+read IP
+
 # generic scan by IP
-nmap IP
+sudo nmap $IP
 
 # SERVICE/VERSION DETECTION
-nmap -sV IP
+sudo nmap -sV $IP
 
-# Generic scan but deeper
-nmap -sS -sU -p 1-1024 -T4 -A -v IP
+# arp ping may be a problem due to the default timeout in namp (honeypot reply may be slow). This command should bypass the problem 
+sudo nmap -sS -sU -Pn --disable-arp-ping -p 1-1024 -T4 -A -v $IP 
 
 # Secific scan for s7 service 
-#nmap --script s7-info.nse -p 102 IP
+sudo nmap --script s7-info.nse -p 102 $IP
 
 # Sepcific scan for modubus services 
-nmap --script modbus-discover.nse --script-args='modbus-discover.aggressive=true' -p 502 IP
+sudo nmap --script modbus-discover.nse --script-args='modbus-discover.aggressive=true' -p 502 $IP
 
 # specific scan for snmp service and obtain the fingerprint of the PLC
 # requires: sudo apt install snmp
-snmpwalk -v1 -c public IP
+snmpwalk -v1 -c public $IP
+
+# check ftp connection
+ftp $IP
+
+# check telnet connection
+telnet $IP
